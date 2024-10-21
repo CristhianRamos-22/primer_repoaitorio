e98861b274eae8163a873543fda45bd9dac8cfbf
CristhianRamos-22 <ara_2210@outlook.com>
2024-10-21 09:55:14 -0700

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,15 @@
+<?php
+/*
+CBTIS 89
+CRISTHIAN MOLINA RAMOS 
+PROGRAMACION 3Aª
+*/
+echo "mi nombres es cristhian molina ramos";
+echo "<br>";
+echo "soy de DGO. durango";
+echo "<br>";
+echo "estudio en el cbtis 89";
+echo "<br>";
+echo " estudio la carrera de programacion en 3ªA T.M";
+
+?>
