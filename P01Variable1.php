ce386b3c8deec8a2ba6dc029491cc475c8989844
CristhianRamos-22 <ara_2210@outlook.com>
2024-10-21 09:55:45 -0700

    Add files via upload

---
+++ P01Variable1.php
@@ -0,0 +1,24 @@
+<?php
+/*CBTIS 89
+programa que sirve para saber el sueldo
+cristhian molina ramos
+programacion T.M.
+*/
+
+$dia=24;
+$sueldo=758.43;
+$nombre= "Juan";
+$exite= true;
+echo "variable entera:";
+echo$dia;echo"<br>";
+echo "variable double:";
+echo $sueldo; echo"<br>";
+echo "variable string";
+echo $nombre;echo"<br>";
+echo "variable boolean";
+echo $exite;
+echo "<br>";
+$cadena1="hola";
+$cadena2="mundo";
+echo $cadena1. " ".$cadena2;
+?>
