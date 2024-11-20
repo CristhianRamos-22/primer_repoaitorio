c57a4e98d7fb6cba7d92fcab521c2fd0612ece0b
CristhianRamos-22 <ara_2210@outlook.com>
2024-11-20 10:37:48 -0600

    Add files via upload

---
+++ Unidad 3/Examen3AMolinaRamos.php
@@ -0,0 +1,16 @@
+<?php
+/*CBTIS 89
+programa que almacena el nombre de cuatro personas en un arreglo $Alumnos y en otro arreglo multidimensiona llamado $Calificaciones almacena las calificaciones de cada uno de ellos por medio de filas y columnas
+cristhian molina ramos
+programacion T.M.
+*/
+$Alumnos = array()
+$Calificaciones = array()
+$Promedio = array()
+$Estatus = array()
+
+$ArrayEstatus = Array("Aprobado", "Reprobado");
+$ArrayAlumnos = Array("Luz", "Fer", "Leo", "Ale");
+
+
+
